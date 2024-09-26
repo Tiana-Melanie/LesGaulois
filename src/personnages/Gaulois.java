a896c7130218f789608d7b5f6b2f349ef15837f5
Tiana Mélanie RABARISON <tiana-melanie.rabarison@epita.fr>
2024-09-26 21:24:22 +0200

    Le gaulois bois la potion

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -24,7 +24,8 @@ public class Gaulois {
 
 	public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-		romain.recevoirCoup(force/3);
+		romain.recevoirCoup((force/3)*effetPotion);
+		
 		
 	}
 	@Override
@@ -32,18 +33,29 @@ public class Gaulois {
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
 	}
 	
+	public int boirePotion(int forcePotion) {
+		effetPotion = forcePotion;
+		parler("Merci Druide, je sens que ma force est "+ forcePotion+" fois décuplée.");
+		return effetPotion;
+	}
+	
 	public static void main(String[] args) {
 		Gaulois asterix;
-		asterix = new Gaulois("Asterix", 8);
+		asterix = new Gaulois("Asterix",8 );
 		
 		Romain minus;
 		minus = new Romain("Minus", 6);
 		
 		System.out.println(asterix.getNom());
 		System.out.println(asterix);
-		System.out.println(asterix.prendreParole());
-		asterix.parler("Bonjour");
+		 
+		//System.out.println(asterix.prendreParole());
+		
+		//asterix.parler("Bonjour");
+		asterix.boirePotion(8);
+		//System.out.println(asterix); verification de l'affectation pour effetPotion
 		asterix.frapper(minus);
+		
 	}
 	
 }
