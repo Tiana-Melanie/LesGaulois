f4edfac086d12862336dbe237ff32a76519ebea2
Tiana Mélanie RABARISON <tiana-melanie.rabarison@epita.fr>
2024-09-26 20:56:08 +0200

    Le druide prépare la potion

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,28 +1,54 @@
 package personnages;
+import java.util.Random;
 
 public class Druide {
 	private String nom;
 	private int effetPotionMin;
 	private int effetPotionMax;
+	private int forcePotion = 1;
 
-	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
+	public Druide(String nom, int effetPotionMin, int effetPotionMax, int forcePotion) {
 		this.nom = nom;
 		this.effetPotionMin = effetPotionMin;
 		this.effetPotionMax = effetPotionMax;
-		parler("Bonjour, je suis le druide" + nom + "et ma potion peut aller d'une force" + effetPotionMin + "à"
+		this.forcePotion = forcePotion;
+		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " à "
 				+ effetPotionMax + ".");
 	}
 
 	public String getNom() {
-		return nom;
+		return "Mon nom est " + nom;
 	}
 
 	public void parler(String texte) {
-		System.out.println(prendreParole() + "'" + texte + "'");
-	}
+			System.out.println(prendreParole() +""+ texte +"");
+		}
+		
 
 	private String prendreParole() {
-		return "Le druide "+ nom + ":";
+		return "Le druide "+ nom + ": ";
 	}
+	
+	public int preparerPotion() {
+		Random valeur= new Random();
+		return forcePotion = valeur.nextInt(effetPotionMin,effetPotionMax);
+	}
+	
+	public void faireParler() {
+		int force = preparerPotion();
+		if (force > 7) {
+			parler("J'ai preparé une super potion de force " + force);
+		}
+		else {
+			parler("Je n'ai pas trouvé tous les ingrédients, ma potion est seulement de force " + force);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Druide Panoramix = new Druide("Panoramix",5,10,1);
+		System.out.println(Panoramix.getNom());
+		Panoramix.faireParler();
+	}
+		
 
 }
